3ba84c0dbd8b1209601f2c9dff25df6c126bc36f
shahrour <ahmadshahrour951@gmail.com>
2021-02-10 02:45:35 +0400

    Level 1 Challenges

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -25,7 +25,38 @@ it("should say hello", function() {
 // 4. Finally see if you would like to refactor your code at all.
 // This is called "Red-Green-Refactor"
 // ========================================================
+it("should return the area of a rectangle", function() {
+  const area_0 = utils.area(5, 10)
+  expect(area_0, '5 * 10 = 50').to.equal(50).that.is.a('number');
 
+  const area_1 = utils.area(0, 0)
+  expect(area_1, '0 * 0 = 0').to.equal(0).that.is.a('number');
+
+  const area_2 = utils.area(1, 1)
+  expect(area_2, '1 * 1 = 1').to.equal(1).that.is.a('number');
+})
+
+it('should return the perimeter of a quadrilateral', function() {
+  const perimeter_0 = utils.perimeter(5, 10)
+  expect(perimeter_0, '5 + 5 + 10 + 10 = 30').to.equal(30).that.is.a('number');
+  
+  const perimeter_1 = utils.perimeter(0, 0);
+  expect(perimeter_1, '0 + 0 + 0 + 0 = 0').to.equal(0).that.is.a('number');
+
+  const perimeter_2 = utils.perimeter(1, 1);
+  expect(perimeter_2, '1 + 1 + 1 + 1 = 4').to.equal(4).that.is.a('number');
+});
+
+it('should return the area of a circle', function() {
+  const circleArea_0 = utils.circleArea(5)
+  expect(circleArea_0, 'pi * 5 * 5 = 78.539.....').to.be.closeTo(78.5398, 0.0001).that.is.a('number');
+
+  const circleArea_1 = utils.circleArea(0);
+  expect(circleArea_1, 'pi * 0 * 0 = 0').to.equal(0).that.is.a('number');
+
+  const circleArea_2 = utils.circleArea(1);
+  expect(circleArea_2, 'pi * 1 * 1 = pi').to.equal(Math.PI).that.is.a('number');
+})
 
 
 
